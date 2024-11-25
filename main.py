2de6ab03874399e2e0574bc43d8f9f8a900105e6
itaykirshner <45193691+itaykirshner@users.noreply.github.com>
2024-11-25 12:26:18 +0200

    Update main.py

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ if not pool:
     exit(1)
 
 
-def patch(node_pool: str, existing_selector: bool) -> base64:
+def patch(node_pool: str, existing_selector: bool, has_affinity: bool) -> base64:
     label, value = node_pool.replace(" ", "").split(":")
     webhook.info(f"Got '{node_pool}' as nodeSelector label, patching...")
 
@@ -29,14 +29,15 @@ def patch(node_pool: str, existing_selector: bool) -> base64:
         patch_operations = [Patch(op="replace", path="/spec/nodeSelector", value={f"{label}": f"{value}"}).dict()]
     else:
         patch_operations = [Patch(op="add", path="/spec/nodeSelector", value={f"{label}": f"{value}"}).dict()]
-    
+
     # remove affinity (to override with_preemtion)
-    patch_operations.append({'op': 'remove', 'path': '/spec/affinity'})
+    if has_affinity:
+        patch_operations.append({'op': 'remove', 'path': '/spec/affinity'})
 
     return base64.b64encode(json.dumps(patch_operations).encode())
 
 
-def admission_review(uid: str, message: str, existing_selector: bool) -> dict:
+def admission_review(uid: str, message: str, existing_selector: bool, has_affinity: bool) -> dict:
     return {
         "apiVersion": "admission.k8s.io/v1",
         "kind": "AdmissionReview",
@@ -45,7 +46,7 @@ def admission_review(uid: str, message: str, existing_selector: bool) -> dict:
             "allowed": True,
             "patchType": "JSONPatch",
             "status": {"message": message},
-            "patch": patch(pool, existing_selector).decode(),
+            "patch": patch(pool, existing_selector, has_affinity).decode(),
         },
     }
 
@@ -57,16 +58,24 @@ def mutate_request(request: dict = Body(...)):
     object_in = request["request"]["object"]
 
     mutate_pod = False
+    has_affinity = False
     try:
         if ("workflows.argoproj.io/workflow" in object_in["metadata"]["labels"]) or (object_in["metadata"]["labels"]["job-type"] == "workflow-runner"):
             mutate_pod = True
+        if "affinity" in object_in["spec"]:
+            has_affinity = True
     except KeyError:
         pass
 
     if mutate_pod:
-        webhook.info(f'Applying nodeSelector for {object_in["kind"]}/{object_in["metadata"]["name"]}.')
+        # notify that the pod is to be mutated. Handle case where "generateName" exists in metadata
+        try:
+            webhook.info(f'Applying nodeSelector for {object_in["kind"]}/{object_in["metadata"]["name"]}.')
+        except KeyError:
+            webhook.info(f'Applying nodeSelector for {object_in["kind"]}/{object_in["metadata"]["generateName"]}.')
         return admission_review(
             uid,
             "Successfully added nodeSelector.",
             True if "nodeSelector" in selector else False,
+            has_affinity,
         )
